b1ee9a0b5a3af43ffab45d50370beb097625bba7
Stephen Zyszkiewicz <stevez@me.com>
2020-04-14 18:34:22 -0500

    v13

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "vapor",
     platforms: [
-	   .iOS(.v8)
+	   .iOS(.v13)
     ],
     products: [
         .library(name: "Vapor", targets: ["Vapor"]),
